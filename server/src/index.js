60c465ff5eeca467001f0ea7cd73da85f4a81aba
AmirNdashi <amiridirisu22@gmail.com>
2026-04-27 15:41:21 +0100

    Prepare app for Railway deployment

--- server/src/index.js
+++ server/src/index.js
@@ -15,7 +15,7 @@ const io = new Server(server, {
     }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -46,6 +46,6 @@ io.on("connection", (socket) => {
 app.set("io", io);
 
 // Start server
-server.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
